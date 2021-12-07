10138e410badcfef8201e2e7aa650d4a0f501563
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-07 14:57:34 +0530

    [vishal] salary Set Event Listener

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,8 @@
+//Salary Range
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() 
+{
+    output.textContent = salary.value;
+});
