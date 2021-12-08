0bd1700116c170718840ba55355c044023526507
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-08 22:02:37 +0530

    [vishal] Perform vallidation aon setter Method Employe

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,19 @@
+//Salary Range
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() 
+{
+    output.textContent = salary.value;
+});
+
+       const text = document.querySelector('#name');
+        const  textError=document.querySelector('.text-Error');
+        text.addEventListener('input',function()
+        {
+          let nameRegex= RegExp('^[A-Z]{1}[a-z]{2,}$');
+          if(nameRegex.test(text.value))
+          textError.textContent="";
+          else textError.textContent="name is incorrect";
+
+        });
